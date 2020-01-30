835fc9cfa3d55a53e0eab370bfdb08daa183d85d
dsr <bdbcat@yahoo.com>
2020-01-29 20:28:14 -0500

    Build for ARM using docker and qemu

---
+++ ci/travis-build-bionic-arm.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+#
+#
+
+# bailout on errors and echo commands.
+set -xe
+sudo apt-get -qq update
+
+#DOCKER_SOCK="unix:///var/run/docker.sock"
+
+#echo "DOCKER_OPTS=\"-H tcp://127.0.0.1:2375 -H $DOCKER_SOCK -s devicemapper\"" \
+#    | sudo tee /etc/default/docker > /dev/null
+#sudo service docker restart;
+#sleep 5;
+#sudo docker pull fedora:31;
+
+#docker run --privileged -d -ti -e "container=docker"  \
+#    -v /sys/fs/cgroup:/sys/fs/cgroup \
+#    -v $(pwd):/opencpn-ci:rw \
+#    fedora:31   /usr/sbin/init
+#DOCKER_CONTAINER_ID=$(docker ps | grep fedora | awk '{print $1}')
+#docker logs $DOCKER_CONTAINER_ID
+#docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
+#    "bash -xe /opencpn-ci/ci/generic-build-mingw.sh 31;
+#         echo -ne \"------\nEND OPENCPN-CI BUILD\n\";"
+#docker ps -a
+#docker stop $DOCKER_CONTAINER_ID
+#docker rm -v $DOCKER_CONTAINER_ID
+
+#sudo apt-get install python3-pip python3-setuptools
+
+docker run --rm --privileged multiarch/qemu-user-static:register --reset
+
+#docker run --rm -t multiarch/ubuntu-debootstrap:arm64-bionic uname -a
+#Linux 28c784e9c7bc 4.4.0-101-generic #124~14.04.1-Ubuntu SMP Fri Nov 10 19:05:36 UTC 2017 aarch64 aarch64 aarch64 GNU/Linux
+
+docker run --rm -t multiarch/ubuntu-debootstrap:armhf-bionic uname -a
+Linux 4de7b4e919b0 4.4.0-101-generic #124~14.04.1-Ubuntu SMP Fri Nov 10 19:05:36 UTC 2017 armv7l armv7l armv7l GNU/Linux
+
+docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
+    "bash -xe /opencpn-ci/ci/travis-build-debian.sh 31;
+         echo -ne \"------\nEND OPENCPN-CI BUILD\n\";"
+docker ps -a
+docker stop $DOCKER_CONTAINER_ID
+docker rm -v $DOCKER_CONTAINER_ID
+
+sudo apt-get install python3-pip python3-setuptools
