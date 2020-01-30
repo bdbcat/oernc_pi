6c50b8ce72116b12b646c0e41dc04439348800ef
dsr <bdbcat@yahoo.com>
2020-01-29 23:12:19 -0500

    Correct build script 5

--- ci/travis-build-bionic-arm.sh
+++ ci/travis-build-bionic-arm.sh
@@ -35,15 +35,29 @@ docker run --rm --privileged multiarch/qemu-user-static:register --reset
 #docker run --rm -t multiarch/ubuntu-debootstrap:arm64-bionic uname -a
 #Linux 28c784e9c7bc 4.4.0-101-generic #124~14.04.1-Ubuntu SMP Fri Nov 10 19:05:36 UTC 2017 aarch64 aarch64 aarch64 GNU/Linux
 
-docker run --rm -t multiarch/ubuntu-debootstrap:armhf-bionic uname -a
-
-docker run --rm -t multiarch/ubuntu-debootstrap:armhf-bionic /bin/bash
-docker ps
-DOCKER_CONTAINER_ID=$(docker ps | grep qemu | awk '{print $1}')
+docker run --privileged -d -ti -e "container=docker"  \
+    multiarch/ubuntu-debootstrap:armhf-bionic /bin/bash
+    
+    
+    
+DOCKER_CONTAINER_ID=$(sudo docker ps | grep armhf-bionic | awk '{print $1}')
 echo $DOCKER_CONTAINER_ID 
+docker exec -ti $DOCKER_CONTAINER_ID pwd
+docker exec -ti $DOCKER_CONTAINER_ID ls
+
+#sudo docker logs $DOCKER_CONTAINER_ID
+#docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
+#    "bash -xe /opencpn-ci/ci/generic-build-mingw.sh 31;
+#         echo -ne \"------\nEND OPENCPN-CI BUILD\n\";"
+
+#sudo docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
+#    "bash -xe /opencpn-ci/ci/travis-build-debian.sh;
+#         echo -ne \"------\nEND OPENCPN-CI BUILD\n\";"
+
 docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
-    "bash -xe /opencpn-ci/ci/travis-build-debian.sh;
+    "bash -xe ci/travis-build-debian.sh;
          echo -ne \"------\nEND OPENCPN-CI BUILD\n\";"
+
 docker ps -a
 docker stop $DOCKER_CONTAINER_ID
 docker rm -v $DOCKER_CONTAINER_ID
