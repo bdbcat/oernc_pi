36b496e2f6110b76606c50d0fcce9cfb055f35ee
dsr <bdbcat@yahoo.com>
2020-01-31 09:38:50 -0500

    Initial implementation of cloudsmith upload

--- ci/travis-build-xenial-armhf.sh
+++ ci/travis-build-xenial-armhf.sh
@@ -43,3 +43,45 @@ docker stop $DOCKER_CONTAINER_ID
 docker rm -v $DOCKER_CONTAINER_ID
 
 sudo apt-get install python3-pip python3-setuptools
+
+#  Upload to cloudsmith
+
+STABLE_REPO=${CLOUDSMITH_STABLE_REPO:-'david-register/ocpn-plugins-stable'}
+UNSTABLE_REPO=${CLOUDSMITH_UNSTABLE_REPO:-'david-register/ocpn-plugins-unstable'}
+
+if [ -z "$CLOUDSMITH_API_KEY" ]; then
+    echo 'Cannot deploy to cloudsmith, missing $CLOUDSMITH_API_KEY'
+    exit 0
+fi
+
+echo "Using \$CLOUDSMITH_API_KEY: ${CLOUDSMITH_API_KEY:0:4}..."
+
+set -xe
+
+python -m ensurepip
+python -m pip install -q setuptools
+python -m pip install -q cloudsmith-cli
+
+BUILD_ID=${APPVEYOR_BUILD_NUMBER:-1}
+commit=$(git rev-parse --short=7 HEAD) || commit="unknown"
+tag=$(git tag --contains HEAD)
+
+echo "Check 1"
+echo $tag
+echo $commit
+
+xml=$(ls *.xml)
+tarball=$(ls *.tar.gz)
+tarball_basename=${tarball##*/}
+
+echo "Check 2"
+echo $tarball_name
+echo $tarball_basename
+
+source ../build/pkg_version.sh
+test -n "$tag" && VERSION="$tag" || VERSION="${VERSION}+${BUILD_ID}.${commit}"
+test -n "$tag" && REPO="$STABLE_REPO" || REPO="$UNSTABLE_REPO"
+tarball_name=oernc-${PKG_TARGET}-${PKG_TARGET_VERSION}-tarball
+
+echo "Check 2"
+echo $tarball_name
