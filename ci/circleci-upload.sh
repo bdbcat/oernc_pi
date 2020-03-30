ce496628d5a47eec41aba6ffb31094576c32b418
dsr <bdbcat@yahoo.com>
2020-03-29 21:41:31 -0400

    Debug Trusty build #3

--- ci/circleci-upload.sh
+++ ci/circleci-upload.sh
@@ -26,6 +26,7 @@ elif dnf --version 2>&1 >/dev/null; then
     sudo dnf install python3-pip python3-setuptools
     sudo python -m pip install -q cloudsmith-cli
 elif apt-get --version 2>&1 >/dev/null; then
+    sudo pip install --upgrade pip 
     sudo  apt install python-setuptools python-pip
     sudo python -m pip install cloudsmith-cli
 
