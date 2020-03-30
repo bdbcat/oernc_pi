921a3446caa0250ab5758a21e3b50802a2b392f2
dsr <bdbcat@yahoo.com>
2020-03-29 20:40:25 -0400

    Test build for Trusty

--- ci/circleci-upload.sh
+++ ci/circleci-upload.sh
@@ -24,7 +24,7 @@ if pyenv versions 2>&1 >/dev/null; then
     pyenv rehash
 elif dnf --version 2>&1 >/dev/null; then
     sudo dnf install python3-pip python3-setuptools
-    sudo python3 -m pip install -q cloudsmith-cli
+    sudo python -m pip install -q cloudsmith-cli
 elif apt-get --version 2>&1 >/dev/null; then
     sudo apt-get install python3-pip python3-setuptools
     sudo python3 -m pip install -q cloudsmith-cli
