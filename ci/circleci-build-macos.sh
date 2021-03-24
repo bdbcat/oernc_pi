c84a189b29f76757461728f76c1fdb3bf89e38a9
dsr <bdbcat@yahoo.com>
2021-03-24 11:13:50 -0400

    Update Travis armhf build scripts #9

--- ci/circleci-build-macos.sh
+++ ci/circleci-build-macos.sh
@@ -9,22 +9,35 @@ set -xe
 
 # As of travis-ci-macos-10.13-xcode9.4.1-1529955246, the travis osx image
 # contains a broken homebrew. Walk-around by reinstalling:
-curl -fsSL \
-     https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh \
-    > uninstall.sh
-chmod 755 uninstall.sh
-./uninstall.sh -q -f
+#curl -fsSL \
+#     https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh \
+#    > uninstall.sh
+#chmod 755 uninstall.sh
+#./uninstall.sh -q -f
 
-inst="https://raw.githubusercontent.com/Homebrew/install/master/install"
-/usr/bin/ruby -e "$(curl -fsSL $inst)"
+#inst="https://raw.githubusercontent.com/Homebrew/install/master/install"
+#/usr/bin/ruby -e "$(curl -fsSL $inst)"
 
-git -C /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core fetch --unshallow
-git -C /usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask fetch --unshallow
 
+#set -o pipefail
+#for pkg in cairo cmake libarchive libexif python3 wget; do
+#    brew list $pkg 2>&1 >/dev/null || brew install $pkg
+#done
+
+# Fix broken ruby on the CircleCI image:
+if [ -n "$CIRCLECI" ]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+set -xe
 
 set -o pipefail
-for pkg in cairo cmake libarchive libexif python3 wget; do
-    brew list $pkg 2>&1 >/dev/null || brew install $pkg
+# Check if the cache is with us. If not, re-install brew.
+brew list --versions libexif || brew update-reset
+
+for pkg in cairo cmake gettext libarchive libexif python wget; do
+    brew list --versions $pkg || brew install $pkg || brew install $pkg || :
+    brew link --overwrite $pkg || brew install $pkg
 done
 
 wget http://opencpn.navnux.org/build_deps/wx312_opencpn50_macos109.tar.xz
