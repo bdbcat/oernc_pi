ecdf0f0b2f080897def408a555e7dac715d15c47
dsr <bdbcat@yahoo.com>
2021-03-24 22:22:15 -0400

    Update Travis armhf build scripts #12

--- ci/travis-build-raspbian-armhf-buster.sh
+++ ci/travis-build-raspbian-armhf-buster.sh
@@ -34,7 +34,7 @@ docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -c 'sudo apt-key add k2'
 docker exec -ti $DOCKER_CONTAINER_ID sudo apt update
 docker exec -ti $DOCKER_CONTAINER_ID echo "------\nEND apt-get update\n" 
 
-docker exec -ti $DOCKER_CONTAINER_ID sudo apt-get -y install git build-essential cmake gettext wx-common libwxgtk3.0-dev libbz2-dev libcurl4-openssl-dev libexpat1-dev libcairo2-dev libarchive-dev liblzma-dev libexif-dev lsb-release 
+docker exec -ti $DOCKER_CONTAINER_ID sudo apt-get -y install git build-essential gettext wx-common libwxgtk3.0-dev libbz2-dev libcurl4-openssl-dev libexpat1-dev libcairo2-dev libarchive-dev liblzma-dev libexif-dev lsb-release 
 
 #Locally built CMake 3.20 included in DOCKER_IMAGE=opencpn/raspbian-buster-cmake320:plugin_build_tooling
 # Fixes bugs in compiler detection.
